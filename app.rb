95be9c56722e5c143084f050794e99242769c273
laxbuddy1 <jcyachimowicz131@gmail.com>
2018-06-26 19:58:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
+require_relative 'config/enviroment'
+
+
+
+
+
+
+
+
+
 get'/:operation/:number1/:number2' do
   @operation=params[:operation]
   @number1=params[:number1].to_i
-  @number2=params[number2].to_i
+  @number2=params[:number2].to_i
 
   if @operation=="add"
     @number1+@number2
